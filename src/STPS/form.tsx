6f58297b4e43e08e8b3719b3cb6f20f34f20e1ef
Alan <Aux.Software06@eservicesmx.com>
2025-10-30 13:12:12 -0700

    feat: implement FormSTPS component and update App to use it; add Button component

---
+++ src/STPS/form.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const FormSTPS = () => {
+  return (
+    <div>
+      Form STPS
+      <Button>Submit</Button>
+    </div>
+  );
+};
+
+export default FormSTPS;
